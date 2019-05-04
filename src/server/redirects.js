4f9dfcd07990e7a8e726b3b35536c12c4fac6504
Tenrys <tenrys.iaido@live.fr>
2019-05-05 00:36:22 +0200

    Lazy snek

--- src/server/redirects.js
+++ src/server/redirects.js
@@ -1,4 +1,4 @@
-let redirects = {
+const redirects = {
     gallery: "https://loadingscreen.metastruct.net/",
     msdnaa: "https://www3.metastruct.net/msdnaa",
     rocket: "https://www3.metastruct.net/rocket",
@@ -6,33 +6,25 @@ let redirects = {
     discord: "https://discord.gg/CHuxFSd",
     webchat: "https://www3.metastruct.net/webchat",
 }
+const joinURLs = {
+    eu1 = "g1.metastruct.net:27015",
+    eu2 = "g2.metastruct.net:27018",
+    hl2coop = "g2.metastruct.net:27016",
+    hl2dm = "g2.metastruct.net:27015"
+}
 
 module.exports = app => {
-    for (const name in redirects) {
-        if (redirects.hasOwnProperty(name)) {
-            const url = redirects[name]
-
-            console.log("Added redirect for " + name + " at " + url)
-            app.get(`/${name}`, (req, res) => {
-                res.redirect(url)
-            })
-        }
+    for (const [name, url] of Object.entries(redirects)) {
+        console.log("Added redirect for " + name + " at " + url)
+        app.get(`/${name}`, (req, res) => {
+            res.redirect(url)
+        })
     }
-    app.get(`/join/eu1/:pwd`, (req, res) => {
-        let pwd = (req.params.pwd || "metawebsite").replace(/[^a-zA-Z*0-9:+-\s]+/g,"")
-        res.redirect("steam://connect/g1.metastruct.net:27015/"+pwd)
-    })
-    app.get(`/join/eu2/:pwd`, (req, res) => {
-        let pwd = (req.params.pwd || "metawebsite").replace(/[^a-zA-Z*0-9:+-\s]+/g,"")
-        res.redirect("steam://connect/g2.metastruct.net:27018/"+pwd)
-    })
-    app.get(`/join/hl2coop/:pwd`, (req, res) => {
-        let pwd = (req.params.pwd || "metawebsite").replace(/[^a-zA-Z*0-9:+-\s]+/g,"")
-        res.redirect("steam://connect/g2.metastruct.net:27016/"+pwd)
-    })
-    app.get(`/join/hl2dm/:pwd`, (req, res) => {
-        let pwd = (req.params.pwd || "metawebsite").replace(/[^a-zA-Z*0-9:+-\s]+/g,"")
-        res.redirect("steam://connect/g2.metastruct.net:27015/"+pwd)
-    })
-
+    for (const [id, hostname] of Object.entries(joinURLs)) {
+        console.log("Added join URL for " + id + " at " + hostname)
+        app.get(`/join/${id}/:pwd`, (req, res) => {
+            let pwd = (req.params.pwd || "metawebsite").replace(/[^a-zA-Z*0-9:+-\s]+/g, "")
+            res.redirect(`steam://connect/${hostname}/${pwd}`)
+        })
+    }
 }
